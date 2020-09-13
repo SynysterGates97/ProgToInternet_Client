478fece70042116f7fcbcd4d1a3f8d08bcf8a58a
Ruslan Hannanov <rusllann97@gmail.com>
2020-09-13 13:59:54 +0500

    Добавил класс для работы с клиентом

---
+++ ProgToInterntet_Client/ProgToInternet_WPF/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+
+namespace ProgToInternet_WPF
+{
+    class Client
+    {
+        IPAddress _serverIp;
+        int _serverPort;
+        IPEndPoint _serverIpEndPoint;
+
+        Socket _connectionSocket;
+
+        public Client(int serverPort = 8000)
+        {
+            _serverPort = serverPort;
+            _connectionSocket = new Socket(AddressFamily.InterNetwork,
+                                               SocketType.Stream,
+                                               ProtocolType.Tcp);
+
+            _serverIp = new IPAddress(new byte[] {0,0,0,0});
+        }
+
+        public string ServerIp
+        {
+            get
+            {
+                if (_serverIp != null)
+                    return _serverIp.ToString();
+                else
+                {
+                    MessageBox.Show("Попытка взятия IP-сервера провалена: IP не задан");
+                    return "0.0.0.0";
+                }
+            }
+            set
+            {
+                _serverIp = IPAddress.Parse(value);
+            }
+        }
+    }
+}
